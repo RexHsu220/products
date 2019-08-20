f9503357fa798525ce09bc79bc3292f16aa20373
Rex Hsu <weiyao220@gmail.com>
2019-08-20 18:07:57 +0800

    add for loop for printing products

--- products.py
+++ products.py
@@ -7,3 +7,7 @@ while True:
 	products.append([name, price]) #create a sub list and add name and price instantly to the sub list, then append this sublist with name and price into products list
 print(products)
 
+for product in products:
+	print(product[0], '的價格為', product[1])
+
+  
